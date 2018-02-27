4c5bb5239ca9e76cbdfce9feddedaafcf59f4851
Nick Main <nickmaster24@gmail.com>
2018-02-27 13:31:28 -0500

    Refactored contentProxy

--- scripts/contentProxy.js
+++ scripts/contentProxy.js
@@ -1,5 +1,10 @@
+function getCurrentDocumentInnerHTML(responseFunction) {
+    let innerHTML = document.documentElement.innerHTML;
+    responseFunction(innerHTML);
+}
+
 chrome.runtime.onMessage.addListener(function (message, sender, responseFunction) {
     if (message === 'get_current_document_inner_HTML') {
-        responseFunction(document.documentElement.innerHTML);
+        getCurrentDocumentInnerHTML(responseFunction);
     }
 });
