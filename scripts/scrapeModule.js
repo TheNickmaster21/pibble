905c0efe9a846d794c96b57b0f78c75b8a0c7590
Nick Main <nickmaster24@gmail.com>
2018-03-01 16:49:32 -0500

    Improved scraping code and parsed input into jQuery nodes

--- scripts/scrapeModule.js
+++ scripts/scrapeModule.js
@@ -1,13 +1,29 @@
-function doStuffWithDocument(innerHTML) {
-    console.log('I received the following DOM content:\n' + innerHTML);
+function sendQueryMessageToActiveTabWithCallback(message, callback) {
+    let activeTabFilter = {active: true, currentWindow: true};
+    chrome.tabs.query(activeTabFilter, function (tabs) {
+        let activeTab = tabs[0];
+        if (activeTab) {
+            chrome.tabs.sendMessage(activeTab.id, message, callback);
+        }
+    });
+}
+
+function useRuleSetToScrapeFromJQueryNodes(ruleSet, nodes) {
+    console.log('Nodes: ' + nodes);
+}
+
+function scrapeDataFromCurrentPage(ruleSet) {
+    sendQueryMessageToActiveTabWithCallback(
+        'get_current_document_inner_HTML',
+        function (innerHTML) {
+            console.log('Me got: ' + innerHTML);
+            let nodes = $.parseHTML(innerHTML);
+            useRuleSetToScrapeFromJQueryNodes(ruleSet, nodes);
+        }
+    );
 }
 
 setInterval(function () {
-    let activeTabFilter = {active: true, currentWindow: true};
-    chrome.tabs.query(activeTabFilter, function (tabs) {
-        let activeTab = tabs[o];
-        if (activeTab) {
-            chrome.tabs.sendMessage(activeTab.id, 'get_current_document_inner_HTML', doStuffWithDocument);
-        }
-    });
-}, 5000);
+    console.log("wat");
+    scrapeDataFromCurrentPage({});
+}, 1000);
