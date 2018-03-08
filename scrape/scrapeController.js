2d9f01fc22950b1dd055d9fb50fb154b8befa406
Nick Main <nickmaster24@gmail.com>
2018-03-08 16:47:59 -0500

    Added more field to scrape for fortune and improve regex capabilities of the scraper

--- scrape/scrapeController.js
+++ scrape/scrapeController.js
@@ -13,9 +13,42 @@ new Vue({
         scrapeResults: [],
         ruleSet: {
             rules: [
-                {
+                { // Ticker
+                    selector: ".branding-tile-ticker",
+                    selectorIndex: 0,
+                    regex: "([^\\s]+)",
+                    regexIndex: 0
+                },
+                { // Rank
+                    selector: "p.branding-tile-rank",
+                    selectorIndex: 0,
+                    regex: "",
+                    regexIndex: 0
+                },
+                { // Share Price
+                    selector: ".branding-tile-ticker",
+                    selectorIndex: 0,
+                    regex: "([^\\s]+)",
+                    regexIndex: 1
+                },
+                { // Revenue
                     selector: ".data[data-reactid*='Revenues ($M)']",
-                    index: 0,
+                    selectorIndex: 0,
+                    regex: ""
+                },
+                { // Revenue Change
+                    selector: ".data[data-reactid*='Revenue Change']",
+                    selectorIndex: 0,
+                    regex: ""
+                },
+                { // Profits
+                    selector: ".data[data-reactid*='Profits ($M)']",
+                    selectorIndex: 0,
+                    regex: ""
+                },
+                { // Profits Change
+                    selector: ".data[data-reactid*='Profit Change']",
+                    selectorIndex: 0,
                     regex: ""
                 }
             ]
