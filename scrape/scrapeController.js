2d9918e52f88a44254ce75249ca0f70bed9050ae
Nick Main <nickmaster24@gmail.com>
2018-03-08 23:23:09 -0500

    Added all fields for Fortune 500 scraping

--- scrape/scrapeController.js
+++ scrape/scrapeController.js
@@ -21,35 +21,77 @@ new Vue({
                 },
                 { // Rank
                     selector: "p.branding-tile-rank",
-                    selectorIndex: 0,
-                    regex: "",
-                    regexIndex: 0
+                    selectorIndex: 0
                 },
-                { // Share Price
-                    selector: ".branding-tile-ticker",
-                    selectorIndex: 0,
-                    regex: "([^\\s]+)",
-                    regexIndex: 1
-                },
+                // { // Share Price
+                //     selector: ".branding-tile-ticker",
+                //     selectorIndex: 0,
+                //     regex: "([^\\s]+)",
+                //     regexIndex: 1
+                // },
                 { // Revenue
                     selector: ".data[data-reactid*='Revenues ($M)']",
-                    selectorIndex: 0,
-                    regex: ""
+                    selectorIndex: 0
                 },
                 { // Revenue Change
                     selector: ".data[data-reactid*='Revenue Change']",
-                    selectorIndex: 0,
-                    regex: ""
+                    selectorIndex: 0
                 },
                 { // Profits
                     selector: ".data[data-reactid*='Profits ($M)']",
-                    selectorIndex: 0,
-                    regex: ""
+                    selectorIndex: 0
                 },
                 { // Profits Change
                     selector: ".data[data-reactid*='Profit Change']",
-                    selectorIndex: 0,
-                    regex: ""
+                    selectorIndex: 0
+                },
+                { // Assets
+                    selector: "td[data-reactid*='company-data-Assets']",
+                    selectorIndex: 1
+                },
+                { // Market Value
+                    selector: "td[data-reactid*='company-data-Market Value']",
+                    selectorIndex: 1
+                },
+                { // Employees
+                    selector: "p[data-reactid*='company-info-card-Employees']",
+                    selectorIndex: 1
+                },
+                { // Previous Rank
+                    selector: ".data[data-reactid*='slide-Previous Rank']",
+                    selectorIndex: 0
+                },
+                { // CEO
+                    selector: "p[data-reactid*='company-info-card-CEO.']",
+                    selectorIndex: 1
+                },
+                { // CEO Title
+                    selector: "p[data-reactid*='company-info-card-CEO Title']",
+                    selectorIndex: 1
+                },
+                { // Sector
+                    selector: "p[data-reactid*='company-info-card-Sector']",
+                    selectorIndex: 1
+                },
+                { // Industry
+                    selector: "p[data-reactid*='company-info-card-Industry']",
+                    selectorIndex: 1
+                },
+                { // HQ Location
+                    selector: "p[data-reactid*='company-info-card-HQ Location']",
+                    selectorIndex: 0
+                },
+                { // Website
+                    selector: "a[data-reactid*='company-info-card-Website']",
+                    selectorIndex: 0
+                },
+                { // Years on Fortune
+                    selector: "p[data-reactid*='company-info-card-Years on Fortune']",
+                    selectorIndex: 1
+                },
+                { // Market Cap
+                    selector: ".ticker-item",
+                    selectorIndex: 1
                 }
             ]
         }
