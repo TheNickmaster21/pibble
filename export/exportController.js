9586d116ea8a5da8cb6e5465ac1716bb078903e3
Sean Hale <seanhale1@live.com>
2018-03-09 09:53:37 -0500

    Created dataview & moved the table to it

--- export/exportController.js
+++ export/exportController.js
@@ -1,3 +1,16 @@
+new Vue({
+    el: '#view-full-data',
+    methods: {
+        dataTab: function () {
+            chrome.tabs.create({'url': chrome.extension.getURL('export/dataView.html')}, function(tab) {
+                // Tab opened.
+            });
+        }
+    }
+});
+
+
+
 // register the grid component
 Vue.component('demo-grid', {
     template: '#grid-template',
