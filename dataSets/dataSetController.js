65b1bf7cfb45c2f272b86efab84cccc74ede1402
Sean Hale <seanhale1@live.com>
2018-03-10 22:24:13 -0500

    basic framework for dataset page

---
+++ dataSets/dataSetController.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#dataSet-page',
+    methods: {
+        newDataSetName: function (newName) {
+
+        },
+        getDataSetNames: function () {
+
+        },
+        isDataSetNameTaken: function () {
+            return false;
+        }
+    },
+    data: {
+        dataSetNames: []
+    }
+});
