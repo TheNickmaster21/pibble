60429a63e3b6c57b267d990914ead2f44f0c9d5b
Nick Main <nickmaster24@gmail.com>
2018-04-05 12:48:30 -0400

    Improved token generation

--- scripts/scrapeModule.js
+++ scripts/scrapeModule.js
@@ -1,11 +1,28 @@
 function parseHtmlDataIntoTokenData($htmlData) {
     let tokens = [];
+    tokens.push({type: 'start'});
+
+    function tokenizeText(text) {
+        tokens.push({type: 'text', value: text})
+    }
+
+    function makeAttributeObject(attributeNodes) {
+        if (!attributeNodes || attributeNodes.length < 1) return null;
+
+        return _.map(attributeNodes,
+            node => {
+                return {name: node.name, value: node.value};
+            });
+    }
 
     function recurseThroughChildren(obj) {
         if (!obj) return;
-        tokens.push({type: obj.localName});
-        if(obj.innerText){
-            obj.innerText //TODO turn inner text into tokens
+        let lastToken = tokens[tokens.length - 1];
+        if (lastToken.type !== obj.localName) {
+            //tokens.push({type: obj.localName, attributes: makeAttributeObject(obj.attributes)});
+        }
+        if (obj.innerText) {
+            tokenizeText(obj.innerText); //TODO turn inner text into tokens
         }
         if (!obj.children) return;
         _.each(obj.children, recurseThroughChildren);
@@ -13,6 +30,7 @@ function parseHtmlDataIntoTokenData($htmlData) {
 
     $htmlData.children().each(function (i, obj) {
         recurseThroughChildren(obj);
+        console.log(obj);
     });
 
     console.log(tokens);
