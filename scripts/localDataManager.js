9fee562df3cb280bdf3a983735c66211eef36335
Nicholas (Nick) Main <nickmaster24@gmail.com>
2018-04-19 18:31:20 -0400

    Update localDataManager.js
    Removed testing console logs

--- scripts/localDataManager.js
+++ scripts/localDataManager.js
@@ -1,6 +1,5 @@
 //load data: Load the JSON data associated with the given key
 function loadData(key) {
-    console.log('load', key, localStorage.getItem(key));
     if (localStorage.getItem(key) != null) {
         return JSON.parse(localStorage.getItem(key));
     }
@@ -9,6 +8,5 @@ function loadData(key) {
 
 //save data: Save the given JSON data to with the given key
 function saveData(key, data) {
-    console.log('save', key, data);
     localStorage.setItem(key, JSON.stringify(data));
 }
